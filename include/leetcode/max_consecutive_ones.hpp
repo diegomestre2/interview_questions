b199bba0129b1b77368103ca7d002faaacdfb141
diegomestre2 <diegogomest@gmail.com>
2020-08-18 20:38:06 +0200

    Adding even digits

--- include/leetcode/max_consecutive_ones.hpp
+++ include/leetcode/max_consecutive_ones.hpp
@@ -19,3 +19,18 @@ void run_max_consecutive_ones() {
 
 	assert(find_max_consecutive_ones(std::vector<int>({1, 0, 1, 1, 0, 1})) == 3);
 }
+
+int findNumbers(std::vector<int> &nums) {
+	size_t result{0}, count{0};
+	for (auto num : nums) {
+		while (num >= 10) {
+			num = num / 10;
+			count++;
+		}
+		if (++count % 2 == 0) {
+			result++;
+		}
+		count = 0;
+	}
+	return result;
+}
