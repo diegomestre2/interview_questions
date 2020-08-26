885c99de1b77cde2da85fc57986d5b3f90ae2a39
diegomestre2 <diegogomest@gmail.com>
2020-08-26 20:38:13 +0200

    Fix issue with inner node

--- include/data_structure/binary_tree.hpp
+++ include/data_structure/binary_tree.hpp
@@ -2,14 +2,24 @@
 
 #include <iostream>
 
+using Value = uint32_t;
+struct Node {
+	Node(Value new_value) {
+		value = new_value;
+		left_node = nullptr;
+		right_node = nullptr;
+	}
+	Value value;
+	Node *left_node;
+	Node *right_node;
+};
 class BinarySearchTree {
 public:
 	BinarySearchTree();
 	BinarySearchTree(BinarySearchTree &rhs){};
 	BinarySearchTree(BinarySearchTree &&rhs) = delete;
 	~BinarySearchTree();
-	struct Node;
-	using Value = uint32_t;
+
 	// methods
 	void insert_node(Node *node_to_insert);
 	void remove_node(Node *node_to_remove);
@@ -21,15 +31,4 @@ private:
 	Node *find_successor_node(Node *root_node);
 	void print_node(Node *root);
 	Node *root_node;
-};
-
-struct BinarySearchTree::Node {
-	Node(Value new_value) {
-		value = new_value;
-		left_node = nullptr;
-		right_node = nullptr;
-	}
-	Value value;
-	Node *left_node;
-	Node *right_node;
 };
