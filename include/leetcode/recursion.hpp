8621ee5c08ed2ac65e0d79231339fd7d97e59e15
diegomestre2 <diegogomest@gmail.com>
2020-08-26 21:04:13 +0200

    Adding reverse string recursive

--- include/leetcode/recursion.hpp
+++ include/leetcode/recursion.hpp
@@ -17,12 +17,3 @@ void reverse_string(std::vector<char> &s) {
 		std::swap(s[i++], s[j--]);
 	}
 }
-
-void reverse_string(std::vector<char> &s) {
-	if (s.size() == 0)
-		return;
-	size_t i{0}, j{s.size() - 1};
-	while (i < j) {
-		std::swap(s[i++], s[j--]);
-	}
-}
