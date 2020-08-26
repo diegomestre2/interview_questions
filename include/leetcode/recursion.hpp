c75fb74d09e4c5b1c11be7b35f7742a547dad57f
diegomestre2 <diegogomest@gmail.com>
2020-08-26 21:04:08 +0200

    Adding reverse string recursive

--- include/leetcode/recursion.hpp
+++ include/leetcode/recursion.hpp
@@ -1,9 +1,28 @@
 #pragma once
 #include <cstdio>
+#include <vector>
 void print_reverse(const char *string) {
 	if (not *string)
 		return;
 
 	print_reverse(string + 1);
 	std::putchar(*string)
+}
+
+void reverse_string(std::vector<char> &s) {
+	if (s.size() == 0)
+		return;
+	size_t i{0}, j{s.size() - 1};
+	while (i < j) {
+		std::swap(s[i++], s[j--]);
+	}
+}
+
+void reverse_string(std::vector<char> &s) {
+	if (s.size() == 0)
+		return;
+	size_t i{0}, j{s.size() - 1};
+	while (i < j) {
+		std::swap(s[i++], s[j--]);
+	}
 }
