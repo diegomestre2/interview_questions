020913ea85f35b10b96b2bfb99249da7159e826b
diegomestre2 <diegogomest@gmail.com>
2020-10-04 19:16:15 +0200

    kth in grammar

--- include/leetcode/recursion.hpp
+++ include/leetcode/recursion.hpp
@@ -191,3 +191,12 @@ ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
 	}
 	return dummy.next;
 }
+
+int kthGrammar(int N, int K) {
+	if (N == 1)
+		return 0;
+	if (K % 2 == 0)
+		return kthGrammar(N - 1, K / 2) == 0 ? 1 : 0;
+	else
+		return kthGrammar(N - 1, (K + 1) / 2) == 0 ? 0 : 1;
+}
