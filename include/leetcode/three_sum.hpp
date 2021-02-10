4e14b57595dbf26d90ba4793d50ffd589c6a62e0
Diego Gomes Tome <diegogomest@gmail.com>
2021-02-10 20:12:23 +0100

    Adding 3 sum

--- include/leetcode/three_sum.hpp
+++ include/leetcode/three_sum.hpp
@@ -1,18 +1,42 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
 vector<vector<int>> threeSum(vector<int> &nums) {
 	vector<vector<int>> result;
-	std::unordered_map<int, int> memo;
 	if (nums.size() == 0)
 		return {};
-	auto target = 0 - nums[0];
-	for (int j = i + 1; j != nums.size(); ++j) {
-		auto complement = target - nums[j];
-		auto it = memo.find(complement);
-		if (it != memo.end()) {
-			result.push_back({nums[0], nums[j], nums[it->second]});
-		} else {
-			memo.insert(std::make_pair(nums[j], j));
+	std::sort(nums.begin(), nums.end());
+	for (size_t i = 0; i != nums.size(); ++i) {
+		if (nums[i] > 0) {
+			break;
+		}
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+		auto value = -nums[i];
+		auto left = i + 1;
+		auto right = nums.size() - 1;
+		while (left < right) {
+			auto two_sum = nums[left] + nums[right];
+			if (two_sum == value) {
+				result.push_back({-value, nums[left], nums[right]});
+				left++;
+				right--;
+			} else if (two_sum > value) {
+				right--;
+				continue;
+			} else if (two_sum < value) {
+				left++;
+				continue;
+			}
+			while (left < right && nums[left] == nums[left - 1])
+				left++;
+			while (left < right && nums[right] == nums[right + 1])
+				right--;
 		}
 	}
-	//}
 	return result;
 }
